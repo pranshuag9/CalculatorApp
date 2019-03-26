418433977a600a54083b025f360c163062d42330
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2019-03-26 17:02:58 +0530

    Add files via upload

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -5,11 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText et;
     private Button b1;
@@ -21,10 +22,16 @@ public class MainActivity extends AppCompatActivity {
     private Button b7;
     private Button b8;
     private Button b9;
+    private Button b0;
     private Button plus;
     private Button sub;
     private Button mul;
     private Button div;
+    private Button mod;
+    private Button power;
+    private Button closBracket;
+    private Button openBracket;
+    private ImageButton del;
     private Button clr;
 
     public static boolean reset = true;
@@ -34,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        typeCast();
+        et.setSelection(et.getText().length());
+        del.setOnClickListener(this);
+    }
+
+    public void typeCast(){
         b1 = findViewById(R.id.btn1);
         b2 = findViewById(R.id.btn2);
         b3 = findViewById(R.id.btn3);
@@ -43,16 +57,32 @@ public class MainActivity extends AppCompatActivity {
         b7 = findViewById(R.id.btn7);
         b8 = findViewById(R.id.btn8);
         b9 = findViewById(R.id.btn9);
+        b0 = findViewById(R.id.btn0);
         plus = findViewById(R.id.btn_plus);
         sub = findViewById(R.id.btn_sub);
         mul = findViewById(R.id.btn_mul);
         div = findViewById(R.id.btn_div);
+        mod =  findViewById(R.id.btn_mod);
+        power = findViewById(R.id.btn_power);
+        openBracket = findViewById(R.id.btn_opening_bracket);
+        closBracket = findViewById(R.id.btn_closing_bracket);
+        del = findViewById(R.id.btn_del);
         clr = findViewById(R.id.btn_clr);
-
         et = findViewById(R.id.editText);
-        et.setSelection(et.getText().length());
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.btn_del){
+            if(!et.getText().toString().equals("")){
+                String value = et.getText().toString();
+                if(value.length()>0){
+                    value = value.substring(0,value.length()-1);
+                    et.setText(value);
+                }
+            }
+        }
+    }
 
     public void editValue(View view) {
 
@@ -65,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
             et.setText(enteredData);
             reset=false;
         } else {
-
             String s = et.getText().toString();
             s = s + enteredData;
             et.setText(s);
@@ -92,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
 
             et.setText(result+"");
             et.setSelection(et.getText().length());
-            reset=true;
+            //reset=true
         }catch (ArithmeticException a){
             et.setText("Error");
         }
